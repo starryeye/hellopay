51bfb9eb7f6c50f86b435fa0a28d3c966dac8b5c
starryeye <see2818@naver.com>
2024-03-20 20:57:11 +0900

    [starryeye, 2024.03.20] - hellopay init
    내용
    - hellopay init

---
+++ hellopay/src/test/java/dev/practice/hellopay/HellopayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.hellopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
