1982014ecd1221e0b7467dbc90d5e5eab547fa34
starryeye <see2818@naver.com>
2024-03-22 23:47:09 +0900

    [starryeye, 2024.03.22] - hellopay, user service register
    내용
    - develop user service register

--- hellopay/member-service/src/main/java/dev/practice/member/adapter/in/web/RegisterMemberController.java
+++ hellopay/member-service/src/main/java/dev/practice/member/adapter/in/web/RegisterMemberController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/members/")
+@RequestMapping("/api/v1/members")
 public class RegisterMemberController {
 
     private final RegisterMemberUseCase useCase;
